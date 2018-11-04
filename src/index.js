e2a64b64c11aa13dcda95c525871b8e8cdcc7a6a
t-fukamizu <takuro.f.1987@gmail.com>
2018-11-05 03:18:33 +0900

    fix connection

--- src/index.js
+++ src/index.js
@@ -11,17 +11,22 @@ const options = {
     parity: 'none',    // パリティなし
     stopBits: 1,
     flowControl: false,  // フロー制御なしにしとく
-    parser: new SerialPort.parsers.Readline('\r\n')
+    parser: new SerialPort.parsers.Readline('\r\n'),
+    autoOpen: false
 }
-const port = new SerialPort(portName, options, (err) => { 
-    console.error("error on connecting");
-    console.error(err);
+const port = new SerialPort(portName, options, (err) => {
+    if (err) { 
+        return console.log('Error: ', err.message);
+    }
 });
 // const parser = port.pipe(new Readline('\r\n'));
 // parser.on('data', console.log)
 port.open(() => {
-    port.on('data', console.log);
+    console.info('open SerialPort');
+    // port.write(':788001000F0000000000000000F8\r\n');
 });
+
+port.on('data', console.log);
 
 // Open errors will be emitted as an error event
 port.on('error', (err) => {
