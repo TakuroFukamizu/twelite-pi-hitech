4bf471379d859b15fdf9143f046ed8f143a2e254
t-fukamizu <takuro.f.1987@gmail.com>
2018-11-05 03:11:03 +0900

    fix open method

--- src/index.js
+++ src/index.js
@@ -10,15 +10,21 @@ const options = {
     dataBits: 8,
     parity: 'none',    // パリティなし
     stopBits: 1,
-    flowControl: false  // フロー制御なしにしとく
+    flowControl: false,  // フロー制御なしにしとく
+    parser: new SerialPort.parsers.Readline('\r\n')
 }
-const port = new SerialPort(portName, options);
-const parser = port.pipe(new Readline('\r\n'));
-
-parser.on('data', console.log)
+const port = new SerialPort(portName, options, (err) => { 
+    console.error("error on connecting");
+    console.error(error);
+});
+// const parser = port.pipe(new Readline('\r\n'));
+// parser.on('data', console.log)
+port.open(() => {
+    port.on('data', console.log);
+});
 
 // Open errors will be emitted as an error event
-port.on('error', function(err) {
+port.on('error', (err) => {
     console.log('Error: ', err.message)
   })
 
